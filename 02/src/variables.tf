a8bcf413e16bf758f31b2ba677ea984728a48ab8
n0rthf1ght3r <31826019+n0rthf1ght3r@users.noreply.github.com>
2025-08-20 22:57:07 +0300

    Update variables.tf

--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -1,5 +1,3 @@
-### cloud vars
-
 variable "cloud_id" {
   type        = string
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
@@ -10,15 +8,15 @@ variable "folder_id" {
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
-variable "service_account_key_file" {
-  type        = string
-  description = "Путь к JSON-ключу сервисного аккаунта"
-}
-
 variable "default_zone" {
   type        = string
   default     = "ru-central1-a"
   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "default_cidr" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
 }
 
 variable "vpc_name" {
@@ -27,41 +25,41 @@ variable "vpc_name" {
   description = "VPC network & subnet name"
 }
 
-variable "default_cidr" {
-  type        = list(string)
-  default     = ["10.0.1.0/24"]
-  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+variable "vms_ssh_root_key" {
+  type        = string
+  default     = "<your_ssh_ed25519_key>"
+  description = "ssh-keygen -t ed25519"
+}
+
+variable "service_account_key_file" {
+  type        = string
+  description = "Path to SA JSON key"
 }
 
 variable "default_cidr_b" {
   type        = list(string)
   default     = ["10.0.2.0/24"]
-  description = "CIDR for ru-central1-b subnet"
+  description = "CIDR for ru-central1-b"
 }
-
-### image
 
 variable "image_family" {
   type        = string
   default     = "ubuntu-2004-lts"
-  description = "Семейство образов для data.yandex_compute_image"
+  description = "Image family"
 }
-
-### ssh vars
 
 variable "vms_ssh_public_root_key" {
   type        = string
-  description = "ubuntu:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJpYbwJI0AnKlZhwxWipXErfFk1LDxmXMpMIgoscd4r2 webinar"
+  description = "Public SSH key line"
 }
 
-# Конфигурация ресурсов ВМ
 variable "vms_resources" {
   type = map(object({
     cores         = number
     memory        = number
     core_fraction = number
   }))
-
+  # Defaults matching a typical setup (adjust in terraform.tfvars if needed)
   default = {
     web = {
       cores         = 2
@@ -74,4 +72,10 @@ variable "vms_resources" {
       core_fraction = 20
     }
   }
+  description = "CPU/memory/core_fraction settings for web and db VMs"
+}
+
+variable "vms_metadata" {
+  type        = map(string)
+  description = "Common metadata for all VMs (e.g., serial-port-enable, ssh-keys)"
 }
