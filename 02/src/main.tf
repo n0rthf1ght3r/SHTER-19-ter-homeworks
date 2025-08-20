11d2831a092df34b4f3594759f11f7a764f93f4f
n0rthf1ght3r <31826019+n0rthf1ght3r@users.noreply.github.com>
2025-08-20 22:35:25 +0300

    Update main.tf

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -1,6 +1,7 @@
 resource "yandex_vpc_network" "develop" {
   name = var.vpc_name
 }
+
 resource "yandex_vpc_subnet" "develop" {
   name           = var.vpc_name
   zone           = var.default_zone
@@ -8,33 +9,39 @@ resource "yandex_vpc_subnet" "develop" {
   v4_cidr_blocks = var.default_cidr
 }
 
-
-
 resource "yandex_vpc_subnet" "develop_b" {
   name           = "${var.vpc_name}-ru-central1-b"
   zone           = var.vm_db_zone
   network_id     = yandex_vpc_network.develop.id
   v4_cidr_blocks = var.default_cidr_b
 }
+
 data "yandex_compute_image" "ubuntu" {
   family = var.image_family
 }
+
 resource "yandex_compute_instance" "platform" {
-  name        = var.vm_web_name
+  name        = local.vm_web_name
   platform_id = var.vm_web_platform_id
+
   resources {
     cores         = var.vm_web_cores
     memory        = var.vm_web_memory
     core_fraction = var.vm_web_core_fraction
   }
+
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.image_id
+      size     = var.vm_web_boot_disk_size
+      type     = var.vm_web_boot_disk_type
     }
   }
+
   scheduling_policy {
     preemptible = var.vm_web_preemptible
   }
+
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
     nat       = var.vm_web_nat
@@ -44,11 +51,10 @@ resource "yandex_compute_instance" "platform" {
     serial-port-enable = 1
     ssh-keys           = "${var.vm_web_ssh_user}:${var.vms_ssh_public_root_key}"
   }
-
 }
 
 resource "yandex_compute_instance" "db" {
-  name        = var.vm_db_name
+  name        = local.vm_db_name
   platform_id = var.vm_db_platform_id
   zone        = var.vm_db_zone
 
@@ -76,7 +82,7 @@ resource "yandex_compute_instance" "db" {
   }
 
   metadata = {
-    ssh-keys           = "${var.vm_db_ssh_user}:${var.vms_ssh_public_root_key}"
     serial-port-enable = 1
+    ssh-keys           = "${var.vm_db_ssh_user}:${var.vms_ssh_public_root_key}"
   }
 }
