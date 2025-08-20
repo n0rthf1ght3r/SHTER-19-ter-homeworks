c64a705975b44da9b6c64cee24db9389a395933a
n0rthf1ght3r <31826019+n0rthf1ght3r@users.noreply.github.com>
2025-08-20 19:52:25 +0300

    Update main.tf

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -9,6 +9,13 @@ resource "yandex_vpc_subnet" "develop" {
 }
 
 
+
+resource "yandex_vpc_subnet" "develop_b" {
+  name           = "${var.vpc_name}-ru-central1-b"
+  zone           = var.vm_db_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.default_cidr_b
+}
 data "yandex_compute_image" "ubuntu" {
   family = var.image_family
 }
@@ -38,4 +45,38 @@ resource "yandex_compute_instance" "platform" {
     ssh-keys           = "${var.vm_web_ssh_user}:${var.vms_ssh_public_root_key}"
   }
 
+}
+
+resource "yandex_compute_instance" "db" {
+  name        = var.vm_db_name
+  platform_id = var.vm_db_platform_id
+  zone        = var.vm_db_zone
+
+  resources {
+    cores         = var.vm_db_cores
+    memory        = var.vm_db_memory
+    core_fraction = var.vm_db_core_fraction
+  }
+
+  scheduling_policy {
+    preemptible = var.vm_db_preemptible
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+      size     = var.vm_db_boot_disk_size
+      type     = var.vm_db_boot_disk_type
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop_b.id
+    nat       = var.vm_db_nat
+  }
+
+  metadata = {
+    ssh-keys           = "${var.vm_db_ssh_user}:${var.vms_ssh_public_root_key}"
+    serial-port-enable = 1
+  }
 }
