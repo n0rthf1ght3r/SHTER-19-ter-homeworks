9f2ec8f72f6083a117e4712857e85fb61bbbe60e
n0rthf1ght3r <31826019+n0rthf1ght3r@users.noreply.github.com>
2025-08-20 19:52:46 +0300

    Update providers.tf

--- 02/src/providers.tf
+++ 02/src/providers.tf
@@ -1,16 +1,19 @@
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
+      version = "0.124.0"
     }
   }
-  required_version = ">=1.5"
+  required_version = ">=1.8.4"
 }
 
 provider "yandex" {
-  # token     = var.token
-  cloud_id                 = var.cloud_id
-  folder_id                = var.folder_id
-  zone                     = var.default_zone
-  service_account_key_file = file("~/.authorized_key.json")
+  cloud_id                 = "b1g3qbsgri9ct1gdgpmt"
+  folder_id                = "b1g7j9h6bjkteg3c776l"
+  service_account_key_file = file("/authorized_key.json")
+  zone                     = "ru-central1-a" #(Optional) 
 }
+
+# You can set TF_LOG to one of the log levels (in order of decreasing verbosity) TRACE, DEBUG, INFO, WARN or ERROR to change the verbosity of the logs.
+# export TF_LOG=DEBUG
