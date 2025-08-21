dbee4b0eb0dd7cc486bde59204ca4001fa4c60b6
n0rthf1ght3r <31826019+n0rthf1ght3r@users.noreply.github.com>
2025-08-21 14:07:00 +0300

    Update providers.tf

--- 03/src/providers.tf
+++ 03/src/providers.tf
@@ -1,7 +1,11 @@
 terraform {
+
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
+    }
+    local = {
+      source = "hashicorp/local"
     }
   }
   required_version = "~>1.8.4"
